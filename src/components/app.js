88675ad30b2fa3909cebff1351828e3f659e6949
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-05-05 20:20:32 +0430

    Add style to app

--- src/components/app.js
+++ src/components/app.js
@@ -8,15 +8,15 @@ import Profile from "./profile";
 class App extends Component {
   render() {
     return (
-      <>
+      <div className="app">
         <GoogleAuth2 clientId="978371680949-7iclvimbtnkiqncnnbf4v7tiibtt5vnp.apps.googleusercontent.com" />
 
-        <SignIn text="Login" classes="login" />
+        <Profile classes="profile" />
 
-        <SignOut text="Logout" classes="logout" />
+        <SignIn text="Login" classes="button" />
 
-        <Profile />
-      </>
+        <SignOut text="Logout" classes="button" />
+      </div>
     );
   }
 }
