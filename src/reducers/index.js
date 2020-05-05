eb676691ab87397ae3044c29f62ce72ca624d558
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-05-05 08:14:31 +0430

    v0.5.0

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
-import { googleAuth2Reducer } from "./googleAuth2";
+import googleAuth2Reducer from "./googleAuth2";
 
 const combinedReducers = combineReducers({
   googleAuth2: googleAuth2Reducer,
