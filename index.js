a5686d4c4b196453c10391a871594ee6ae7f197a
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-05-05 19:27:18 +0430

    Change root index file

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-import component from "./dist/components/googleAuth2";
+import _GoogleAuth2 from "./dist/components/googleAuth2";
+import _SignIn from "./dist/components/signIn";
+import _SignOut from "./dist/components/signOut";
+import _Card from "./dist/components/card";
 import reducer from "./dist/reducers/googleAuth2";
 
-export { component as GoogleAuth2 };
+export { _GoogleAuth2 as GoogleAuth2 };
+export { _SignIn as SignIn };
+export { _SignOut as SignOut };
+export { _Card as Card };
 export { reducer as googleAuth2Reducer };
