f863024b1959fbcdc0c9ee5b66344fdc42e1b310
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-05-12 20:37:27 +0430

    Improve named exports

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-import _GoogleAuth2 from "./dist/components/googleAuth2";
-import _SignIn from "./dist/components/signIn";
-import _SignOut from "./dist/components/signOut";
-import _Profile from "./dist/components/profile";
-import reducer from "./dist/reducers/googleAuth2";
-
-export { _GoogleAuth2 as GoogleAuth2 };
-export { _SignIn as SignIn };
-export { _SignOut as SignOut };
-export { _Profile as Profile };
-export { reducer as googleAuth2Reducer };
+export { default as GoogleAuth2 } from "./dist/components/googleAuth2";
+export { default as SignIn } from "./dist/components/signIn";
+export { default as SignOut } from "./dist/components/signOut";
+export { default as Profile } from "./dist/components/profile";
+export { default as googleAuth2Reducer } from "./dist/reducers/googleAuth2";
