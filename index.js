14dfd6a56c5aff8eac24ba95affaf518b6013b9e
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-12-29 13:24:57 +0330

    Ammend exports

--- index.js
+++ index.js
@@ -2,7 +2,4 @@ export { default as GoogleAuth2 } from "./dist/components/GoogleAuth2";
 export { default as SignIn } from "./dist/components/SignIn";
 export { default as SignOut } from "./dist/components/SignOut";
 export { default as Profile } from "./dist/components/Profile";
-export {
-  default as googleAuth2Reducer,
-  IStateGoogleAuth2,
-} from "./dist/reducers/googleAuth2";
+export { default as googleAuth2Reducer } from "./dist/reducers/googleAuth2";
